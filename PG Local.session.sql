da29ee933ed7e25adf3a6513163121ed8ac1157a
wyattwong-git <wongwyattl@gmail.com>
2025-08-21 22:33:46 -0400

    uploaded files

--- PG Local.session.sql
+++ PG Local.session.sql
@@ -1,2 +1,17 @@
-# Display the contents of the 'fintech_ltv' table
+--# Display the contents of the 'fintech_ltv' table
 SELECT * FROM fintech_ltv;
+
+--# Count different number of values for payment methods
+SELECT 
+    preferred_payment_method,
+    COUNT(*)
+FROM fintech_ltv
+GROUP BY preferred_payment_method
+ORDER BY COUNT(*) DESC;
+
+--# Display the highest total transaction value for each payment method
+SELECT
+    preferred_payment_method,
+    MAX(total_spent) AS max_transaction_value
+FROM fintech_ltv
+GROUP BY preferred_payment_method;
